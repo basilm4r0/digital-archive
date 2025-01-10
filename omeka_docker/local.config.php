9c6bf2a9bfcc9cd740565be32bc613eacf898873
Basil Mari <basilm4r0@gmail.com>
2025-01-10 15:09:56 +0200

    Add SMTP Email configuration template

--- omeka_docker/local.config.php
+++ omeka_docker/local.config.php
@@ -30,4 +30,21 @@ return [
             'Omeka\File\Thumbnailer' => 'Omeka\File\Thumbnailer\ImageMagick',
         ],
     ],
+    'mail' => [
+        'transport' => [
+            'type' => 'smtp',
+            'options' => [
+                'name' => 'localhost',
+                'host' => '127.0.0.1',
+                'port' => 587, // 465 for 'ssl', and 587 for 'tls'
+                'connection_class' => 'login', // 'plain', 'login', or 'crammd5'
+                'connection_config' => [
+                    'username' => null,
+                    'password' => null,
+                    'ssl' => null, // 'ssl' or 'tls'
+                    'use_complete_quit' => true,
+                ],
+            ],
+        ],
+    ],
 ];
